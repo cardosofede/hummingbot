c417c182993f8c261993ea50d4823efbb040d825
TheHolyRoger <theholyroger@theholyroger.com>
2023-06-28 11:51:11 +0100

    Revert other unittest changes

--- test/hummingbot/connector/exchange/mexc/test_mexc_api_order_book_data_source.py
+++ test/hummingbot/connector/exchange/mexc/test_mexc_api_order_book_data_source.py
@@ -79,7 +79,6 @@ class MexcAPIOrderBookDataSourceUnitTests(unittest.TestCase):
 
         self.assertEqual(results[self.trading_pair], 57902.52)
 
-    # @unittest.skip("Test with error")
     @aioresponses()
     def test_fetch_trading_pairs_with_error_status_in_response(self, mock_api):
         mock_response = {}
